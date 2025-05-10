bd04813b36003b9ac932e2386263358fe7472c7e
Vaida <developer@vaidaf.net>
2025-05-11 01:01:46 +1000

    resolved leaks

--- Package.swift
+++ Package.swift
@@ -23,11 +23,9 @@ let package = Package(
         .package(url: "https://www.github.com/Vaida12345/FinderItem", from: "1.0.0"),
         .package(url: "https://www.github.com/Vaida12345/ConcurrentStream", from: "0.1.0"),
         .package(url: "https://www.github.com/Vaida12345/NativeImage", from: "1.0.0"),
-        .package(url: "https://www.github.com/Vaida12345/Optimization", from: "1.0.0")
+        .package(url: "https://www.github.com/Vaida12345/Optimization", from: "1.0.5")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "MIDIKit",
             dependencies: ["DetailedDescription", "FinderItem", "ConcurrentStream", "NativeImage", "Optimization"],
