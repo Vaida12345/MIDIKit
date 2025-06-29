92faa32ec96d396f9acd94fc2b60878b60964d07
Vaida <developer@vaidaf.net>
2025-06-29 13:17:03 +1000

    updates dependencies

--- Package.swift
+++ Package.swift
@@ -19,16 +19,17 @@ let package = Package(
             targets: ["MIDIKit"]),
     ],
     dependencies: [
-        .package(url: "https://www.github.com/Vaida12345/DetailedDescription", from: "2.0.3"),
-        .package(url: "https://www.github.com/Vaida12345/FinderItem", from: "1.0.0"),
+        .package(url: "https://www.github.com/Vaida12345/DetailedDescription", from: "2.0.5"),
+        .package(url: "https://www.github.com/Vaida12345/FinderItem", from: "1.0.16"),
         .package(url: "https://www.github.com/Vaida12345/ConcurrentStream", from: "1.0.1"),
-        .package(url: "https://www.github.com/Vaida12345/NativeImage", from: "1.0.0"),
-        .package(url: "https://www.github.com/Vaida12345/Optimization", from: "1.0.5")
+        .package(url: "https://www.github.com/Vaida12345/NativeImage", from: "1.0.2"),
+        .package(url: "https://www.github.com/Vaida12345/Optimization", from: "1.0.8"),
+        .package(url: "https://www.github.com/Vaida12345/Essentials", from: "1.1.1")
     ],
     targets: [
         .target(
             name: "MIDIKit",
-            dependencies: ["DetailedDescription", "FinderItem", "ConcurrentStream", "NativeImage", "Optimization"],
+            dependencies: ["DetailedDescription", "FinderItem", "ConcurrentStream", "NativeImage", "Optimization", "Essentials"],
             path: "Sources"
         ),
         .executableTarget(name: "Client", dependencies: ["MIDIKit"], path: "Client"),
