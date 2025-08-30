fe4bbb1372b5ed312cbddd507671641869e7d502
Vaida <developer@vaidaf.net>
2025-08-30 18:46:32 +1000

    bump dependencies

--- Package.swift
+++ Package.swift
@@ -19,12 +19,12 @@ let package = Package(
             targets: ["MIDIKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/Vaida12345/DetailedDescription.git", from: "2.0.5"),
-        .package(url: "https://github.com/Vaida12345/FinderItem.git", from: "1.0.16"),
-        .package(url: "https://github.com/Vaida12345/ConcurrentStream.git", from: "1.0.1"),
-        .package(url: "https://github.com/Vaida12345/NativeImage.git", from: "1.0.2"),
-        .package(url: "https://github.com/Vaida12345/Optimization.git", from: "1.0.8"),
-        .package(url: "https://github.com/Vaida12345/Essentials.git", from: "1.1.1")
+        .package(url: "https://github.com/Vaida12345/DetailedDescription.git", from: "2.1.1"),
+        .package(url: "https://github.com/Vaida12345/FinderItem.git", from: "1.2.1"),
+        .package(url: "https://github.com/Vaida12345/ConcurrentStream.git", from: "1.0.2"),
+        .package(url: "https://github.com/Vaida12345/NativeImage.git", from: "1.0.3"),
+        .package(url: "https://github.com/Vaida12345/Optimization.git", from: "1.0.10"),
+        .package(url: "https://github.com/Vaida12345/Essentials.git", from: "1.1.4")
     ],
     targets: [
         .target(
